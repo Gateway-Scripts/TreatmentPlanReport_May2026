ca33df2bcab4b16ecef1761661592fb4fa23eb86
mattcschmidt <mattcschmidt@hotmail.com>
2026-05-18 16:47:42 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TreatmentPlanReport.ViewModels;
+using TreatmentPlanReport.Views;
+using esapi = VMS.TPS.Common.Model.API;
+
+namespace TreatmentPlanReport
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //initialize ESAPI. 
+            //wrap this code in a try/catch to handle any exceptions that may occur.
+            //remember to include a using block around the application creation.
+            try
+            {
+                //after using completes, the app object will call Dispose();
+                using (var app = esapi.Application.CreateApplication())
+                {
+                    //open a patient course and plan, then launch the application.
+                    //default for now.
+                    esapi.Patient patient = app.OpenPatientById("RapidPlan-01");
+                    esapi.Course course = patient.Courses.FirstOrDefault(c => c.Id == "Demo");
+                    esapi.PlanSetup plan = course.PlanSetups.FirstOrDefault(p => p.Id == "IMRT Calc");
+                    var mainView = new MainView();
+                    var mainViewModel = new MainViewModel(plan);
+                    //tell the mainView to evaluate binding expressions using properties of the MainViewModel class.
+                    mainView.DataContext = mainViewModel;
+                    //show the UI.
+                    mainView.ShowDialog();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace TreatmentPlanReport.ViewModels
+{
+    public class MainViewModel
+    {
+        public string PlanId { get; set; }
+        public MainViewModel(PlanSetup plan)
+        {
+            PlanId = plan.Id;
+        }
+    }
+}

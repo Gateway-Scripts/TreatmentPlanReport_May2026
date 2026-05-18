ea6ddbd742d9ea041088070b51c415500130ece8
mattcschmidt <mattcschmidt@hotmail.com>
2026-05-18 19:05:39 +0000

    Add PlanView and PlanViewModel for plan details display
    Introduced PlanViewModel to encapsulate plan properties and created a corresponding PlanView user control for displaying plan information. Updated MainViewModel and MainView.xaml to integrate the new plan view. Modified the project file to include all new components.

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -9,12 +9,12 @@ namespace TreatmentPlanReport.ViewModels
 {
     public class MainViewModel
     {
-        public string PlanId { get; set; }
         public PatientViewModel LocalPatientViewModel { get; set; }
+        public PlanViewModel LocalPlanViewModel { get; set; }
         public MainViewModel(Patient patient, PlanSetup plan)
         {
-            PlanId = plan.Id;
             LocalPatientViewModel = new PatientViewModel(patient);
+            LocalPlanViewModel = new PlanViewModel(plan);
         }
     }
 }
---
+++ ViewModels/PlanViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace TreatmentPlanReport.ViewModels
+{
+    public class PlanViewModel
+    {
+        //Properties
+        public string PlanId { get; set; }
+        public string PlanName { get; set; }
+        public string CourseId { get; set; }
+        public string ApprovalStatus { get; set; }
+        public string PlanType { get; set; }
+        public string PlanIntent { get; set; }
+
+        //Constructor to set plan information.
+        public PlanViewModel(PlanSetup plan)
+        {
+            PlanId = plan.Id;
+            PlanName = plan.Name;
+            CourseId = plan.Course.Id;
+            ApprovalStatus = plan.ApprovalStatus.ToString();
+            PlanType = plan.PlanType.ToString();
+            PlanIntent = plan.PlanIntent.ToString();
+        }
+    }
+}

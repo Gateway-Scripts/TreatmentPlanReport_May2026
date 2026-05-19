dadd26414b5aeb92a9402e14f2b7c6bdb2b44892
mattcschmidt <mattcschmidt@hotmail.com>
2026-05-19 21:21:24 +0000

    Add print report command and RelayCommand support
    Implemented RelayCommand for command binding in ViewModel. Added PrintReportCommand to MainViewModel to generate and print a FlowDocument report. Updated MainView.xaml with "Generate Report" and "Post to ARIA" buttons. Included new RelayCommand.cs in project and added required WPF using directives.

---
+++ Helpers/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TreatmentPlanReport.Helpers
+{
+    // RelayCommand class to handle command binding in ViewModel.
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        // Event that gets fired when CanExecute changes.
+        public event EventHandler CanExecuteChanged;
+
+        // Constructor for commands without a condition.
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        // Constructor for commands with a condition.
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        // Determines if the command can be executed.
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        // Executes the command action.
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+        // Raises the CanExecuteChanged event to notify the UI of state changes.
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OxyPlot.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
 using TreatmentPlanReport.Helpers;
 using TreatmentPlanReport.Views;
 using VMS.TPS.Common.Model.API;
@@ -18,6 +22,7 @@ namespace TreatmentPlanReport.ViewModels
         public FieldViewModel LocalFieldViewModel { get; set; }
         public DVHViewModel LocalDVHViewModel { get; set; }
         public IsoOrthogonalViewModel LocalOrthogonalImageViewModel { get; set; }
+        public RelayCommand PrintReportCommand { get; private set; }
         public MainViewModel(Patient patient, PlanSetup plan)
         {
             LocalPatientViewModel = new PatientViewModel(patient);
@@ -29,6 +34,56 @@ namespace TreatmentPlanReport.ViewModels
             EventHelper eventHelper = new EventHelper();
             LocalDVHViewModel = new DVHViewModel(plan, eventHelper);
             LocalOrthogonalImageViewModel = new IsoOrthogonalViewModel(patient, plan.Course, plan);
+            PrintReportCommand = new RelayCommand(OnPrint);
+        }
+
+        private void OnPrint(object obj)
+        {
+            FlowDocument fd = new FlowDocument { FontSize = 12, FontFamily = new System.Windows.Media.FontFamily("Calibri") };
+            fd.Blocks.Add(new Paragraph(new Run("Treatment Plan Report")));
+            fd.Blocks.Add(new Paragraph(new Run("Patient Info:")) { FontWeight = FontWeights.Bold });
+            fd.Blocks.Add(new BlockUIContainer(new PatientView { DataContext = LocalPatientViewModel }));
+            fd.Blocks.Add(new Paragraph(new Run("Plan Info:")) { FontWeight = FontWeights.Bold });
+            fd.Blocks.Add(new BlockUIContainer(new PlanView { DataContext = LocalPlanViewModel }));
+            fd.Blocks.Add(new Paragraph(new Run("Prescription:")) { FontWeight = FontWeights.Bold });
+            fd.Blocks.Add(new BlockUIContainer(new RxView { DataContext = LocalRxViewModel }));
+            //fd.Blocks.Add(new Paragraph(new Run("Reference Point:")) { FontWeight = FontWeights.Bold });
+            //fd.Blocks.Add(new BlockUIContainer(new ReferencePointView { DataContext = ReferencePointViewModel }));
+            fd.Blocks.Add(new Paragraph(new Run("Patient Shifts:")) { FontWeight = FontWeights.Bold });
+            fd.Blocks.Add(new BlockUIContainer(new PatientShiftView { DataContext = LocalPatientShiftViewModel }));
+            //foreach (var field in FieldViewModel.Fields)
+            //{
+            //    fd.Blocks.Add(new BlockUIContainer(new FieldDetailsView { DataContext = field }));
+            //}
+            Section fieldSection = new Section { BreakPageBefore = true };
+            fieldSection.Blocks.Add(new Paragraph(new Run("Field Info:")) { FontWeight = FontWeights.Bold });
+            fieldSection.Blocks.Add(new BlockUIContainer(new FieldSummaryView { DataContext = LocalFieldViewModel }));
+            fd.Blocks.Add(fieldSection);
+
+            BitmapSource bmp = new PngExporter().ExportToBitmap(LocalDVHViewModel.DVHPlot);
+            fd.Blocks.Add(new BlockUIContainer(new System.Windows.Controls.Image
+            {
+                Source = bmp,
+                Height = 600,
+                Width = 725
+            }));
+            fd.Blocks.Add(new BlockUIContainer(new System.Windows.Controls.Image { Source = LocalOrthogonalImageViewModel.TransverseImage }));
+            fd.Blocks.Add(new BlockUIContainer(new System.Windows.Controls.Image { Source = LocalOrthogonalImageViewModel.FrontalImage ,
+                    Margin = new System.Windows.Thickness(10)}));
+            fd.Blocks.Add(new BlockUIContainer(new System.Windows.Controls.Image { Source = LocalOrthogonalImageViewModel.SagittalImage,
+                    Margin= new System.Windows.Thickness(10)}));
+            System.Windows.Controls.PrintDialog printer = new System.Windows.Controls.PrintDialog();
+            //printer.PrintTicket.PageOrientation = System.Printing.PageOrientation.Landscape;
+            fd.PageHeight = 1056;
+            fd.PageWidth = 816;
+            fd.PagePadding = new System.Windows.Thickness(50);
+            fd.ColumnGap = 0;
+            fd.ColumnWidth = 816;
+            IDocumentPaginatorSource source = fd;
+            if (printer.ShowDialog() == true)
+            {
+                printer.PrintDocument(source.DocumentPaginator, "TreatmentPlanReport");
+            }
         }
     }
 }
